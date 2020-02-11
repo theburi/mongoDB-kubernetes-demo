0a9da23e85d7026e3df942c5087a62c110981d25
Andrey Belik <belika@hotmail.com>
2020-02-11 10:26:18 +0000

    Amending examples

--- opsmanager/opsmanageruser.sh
+++ opsmanager/opsmanageruser.sh
@@ -1,3 +1,5 @@
 kubectl create secret generic "adminopsmanager" --from-literal=Username="test@test.com" --from-literal=Password='<password>' --from-literal=FirstName="First" --from-literal=LastName="Last"
 
-kubectl create secret generic om-jane-doe-credentials  --from-literal="user=<public key>" --from-literal="publicApiKey=<private key>"
+Open the Ops Manager application. In the UI, generate a new API key by selecting: “UserName -> Account -> Public API Access”
+
+kubectl create secret generic om-jane-doe-credentials  --from-literal="user=<user.email>" --from-literal="publicApiKey=<private key>"
