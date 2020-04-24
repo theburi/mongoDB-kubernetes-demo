5e534d051f710f5f30da29bc56475f3afbfa6326
Andrey Belik <belika@hotmail.com>
2020-04-24 21:29:26 +0100

    Adding LDAP working script

--- LDAP/applyLDAP.py
+++ LDAP/applyLDAP.py
@@ -6,11 +6,11 @@ def getLdapConfigForOm():
   return {
     # Secret
     'bindQueryPassword' : 'password',
-    'bindQueryUser'     : 'cn=read-only-admin,dc=example,dc=com',
+    'bindQueryUser'     : 'cn=admin,dc=ldap,dc=mongodb,dc=local',
     # ConfigMap
     'authzQueryTemplate' : '{USER}?memberOf?base',
-    'servers'            : 'ldap.forumsys.com',
-    'userToDNMapping'    : '[ {  match : "(.+)", substitution: "uid={0},dc=example,dc=com"  }  ]'  } 
+    'servers'            : 'кувфсеув:389',
+    'userToDNMapping'    : '[ {  "ldapQuery": "ou=users,dc=ldap,dc=mongodb,dc=local??sub?(uid={0})", "match" : "(.+)" }  ]'  } 
 #   if isEnabled(configParameter('LDAP_ENABLED')) else None
 
 def getLDAPRoles():
@@ -18,7 +18,7 @@ def getLDAPRoles():
     'authenticationRestrictions' : [],
     'privileges'                 : [],
     'db'         : 'admin',
-    'role'       : f'CN=mathematicians,DN=com',
+    'role'       : 'cn=admins,ou=groups,dc=ldap,dc=mongodb,dc=local',
     'roles'      : [
       {
         'db'   : 'admin',
@@ -29,6 +29,9 @@ def getLDAPRoles():
       }, {
         'db'   : 'admin',
         'role' : 'readWriteAnyDatabase'
+      }, {
+        'db' : 'admin',
+        'role': 'clusterMonitor'
       }
     ]
   }]
@@ -40,65 +43,70 @@ def enableAuthMechanismsForProject(groupId, ldapConfig, ldapRoles):
         """
         url = f'http://a55fbb5a2848f11eaaa090214ecb1cab-857889018.eu-west-1.elb.amazonaws.com:8080/api/public/v1.0/groups/{group}/automationConfig'
         print (url)
-        r = requests.get(url, auth=HTTPDigestAuth('LNTMCKJX', 'e71aec97-903a-4d61-a9fa-ceb715dc8cc5'))
+        r = requests.get(url, auth=HTTPDigestAuth('LNTMCKJX', 'e7'))
+        print ("Getting Automation Config")
         print (r)
         return r.json()
 
     def putAutomationConfig(group, conf):
+          print ('Removing External policy')
           policyurl = f'http://a55fbb5a2848f11eaaa090214ecb1cab-857889018.eu-west-1.elb.amazonaws.com:8080/api/public/v1.0/groups/{group}/controlledFeature'
-          r_pol = requests.put(policyurl, headers={'Content-Type': 'application/json'}, auth=HTTPDigestAuth('LNTMCKJX', 'e71aec97-903a-4d61-a9fa-ceb715dc8cc5'), json= {"policies": [],"externalManagementSystem": { "name": "Kubernetes Operator" }})
+          r_pol = requests.put(policyurl, headers={'Content-Type': 'application/json'}, auth=HTTPDigestAuth('LNTMCKJX', ''), json= {"policies": [],"externalManagementSystem": { "name": "Kubernetes Operator" }})
           print (r_pol.json())
+          print ('Patching Automation config')
           url = f'http://a55fbb5a2848f11eaaa090214ecb1cab-857889018.eu-west-1.elb.amazonaws.com:8080/api/public/v1.0/groups/{group}/automationConfig'
-          r = requests.put(url, headers={'Content-Type': 'application/json'}, auth=HTTPDigestAuth('LNTMCKJX', 'e71aec97-903a-4d61-a9fa-ceb715dc8cc5'), json=conf)
+          r = requests.put(url, headers={'Content-Type': 'application/json'}, auth=HTTPDigestAuth('LNTMCKJX', ''), json=conf)
+          
           return r
 
     # Retrieve automation config
     conf = getAutomationConfig(groupId)
-    conf_old=conf
-    if conf.get('auth')['disabled']:
+    # if conf.get('auth')['disabled']:
       # Authentication in OpsManager Project is not enabled yet, enabling it...
-      key     = 'alkdjlaksdjalksdjlaksjdlaksdjlkJLK234lkajsdlkajsldkajlkjsadasds'
-      initPwd = 'askldalkdjlasdjakjkle4askdjlvn'
-      conf.get('auth')['disabled']                 = False
-      conf.get('auth')['authoritativeSet']         = True
-      # conf.get('auth')['autoAuthMechanisms']        = ['SCRAM-SHA-256', 'MONGODB-CR']
-      conf.get('auth')['autoAuthMechanisms']       = [ 'MONGODB-CR' ]
-      conf.get('auth')['deploymentAuthMechanisms'] = [ 'PLAIN' ]
-      conf.get('auth')['keyfile']                  = '/var/lib/mongodb-mms-automation/keyfile'
-      conf.get('auth')['keyfileWindows']           = '%SystemDrive%\\MMSAutomation\\versions\\keyfile'
-      conf.get('auth')['autoUser']    = 'riemann'
-      conf.get('auth')['autoPwd']     = 'password'
-      conf.get('auth')['key']         = key
-      conf.get('auth')['usersWanted'] = [
-        {
-          'db'      : 'admin',
-          'initPwd' : f'{initPwd}',
-          'user'    : 'mms-monitoring-agent',
-          'roles'   : [{ 'db': 'admin', 'role': 'clusterMonitor'}]
-        }, {
-          'db'      : 'admin',
-          'initPwd' : f'{initPwd}',
-          'user'    : 'mms-backup-agent',
-          'roles'   : [
-            { 'db': 'local', 'role': 'readWrite'},
-            { 'db': 'admin', 'role': 'clusterAdmin'},
-            { 'db': 'admin', 'role': 'readAnyDatabase'},
-            { 'db': 'admin', 'role': 'userAdminAnyDatabase'}
-          ]
-        }
-      ]
-      conf['ldap'] = always_merger.merge({
-          'validateLDAPServerConfig' : True,
-          'bindMethod'               : 'simple',
-          'transportSecurity'        : 'tls',
-          'bindSaslMechanisms'       : ''
-      }, ldapConfig)
-      conf['roles'] = getLDAPRoles()
-      # update the automation config
-      #   print (conf)
-      res = putAutomationConfig(groupId, conf)
+    key = 'alkdjlaksdjalksdjlaksjdlaksdjlkJLK234lkajsdlkajsldkajlkjsadasds'
+    # initPwd = 'askldalkdjlasdjakjkle4askdjlvn'
+    conf.get('auth')['disabled'] = False
+    conf.get('auth')['authoritativeSet'] = True
+    # conf.get('auth')['autoAuthMechanisms']        = ['SCRAM-SHA-256', 'MONGODB-CR']
+    conf.get('auth')['autoAuthMechanisms'] = ['PLAIN']
+    conf.get('auth')['deploymentAuthMechanisms'] = ['PLAIN']
+    conf.get('auth')['keyfile'] = '/var/lib/mongodb-mms-automation/keyfile'
+    conf.get('auth')[
+        'keyfileWindows'] = '%SystemDrive%\\MMSAutomation\\versions\\keyfile'
+    conf.get('auth')['autoUser'] = 'mms-automation-agent'
+    # conf.get('auth')['autoPwd'] = 'password'
+    conf.get('auth')['autoLdapGroupDN'] = 'cn=admins,ou=groups,dc=ldap,dc=mongodb,dc=local'
+    conf.get('auth')['key'] = key
+    # conf.get('auth')['usersWanted'] = [
+    #     {
+    #         'db': 'admin',
+    #         'initPwd': f'{initPwd}',
+    #         'user': 'mms-monitoring-agent',
+    #         'roles': [{'db': 'admin', 'role': 'clusterMonitor'}]
+    #     }, {
+    #         'db': 'admin',
+    #         'initPwd': f'{initPwd}',
+    #         'user': 'mms-backup-agent',
+    #         'roles': [
+    #               {'db': 'local', 'role': 'readWrite'},
+    #               {'db': 'admin', 'role': 'clusterAdmin'},
+    #               {'db': 'admin', 'role': 'readAnyDatabase'},
+    #               {'db': 'admin', 'role': 'userAdminAnyDatabase'}
+    #         ]
+    #     }
+    # ]
+    conf['ldap'] = always_merger.merge({
+        'validateLDAPServerConfig': True,
+        'bindMethod': 'simple',
+        'transportSecurity': 'none',
+        'bindSaslMechanisms': ''
+    }, ldapConfig)
+    conf['roles'] = getLDAPRoles()
+    # update the automation config
+    #   print (conf)
+    res = putAutomationConfig(groupId, conf)
 
-      return res
+    return res
 
 
 
